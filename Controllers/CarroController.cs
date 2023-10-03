473a9aeaeb9229ba4c6ca996f277d064ea58d45d
yStardustzz <artplays34@gmail.com>
2023-10-03 20:01:24 -0300

    push1

---
+++ Controllers/CarroController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class CarroController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var carro = CarroModel.CriarLista();
+            return View(carro);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var carro = CarroModel.CriarLista()[id];
+            return View(carro);
+        }
+
+       
+        public ActionResult Create(int id)
+        {
+            var carro = CarroModel.CriarLista()[id];
+            return View(carro);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+         
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+        public ActionResult Edit(int id)
+        {
+            var carro = CarroModel.CriarLista()[id];
+            return View(carro);
+            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+      
+        public ActionResult Delete(int id)
+        {
+            var carro = CarroModel.CriarLista()[id];
+            return View(carro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+              
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/CarroModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CarroModel
+    {
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public DateTime Fabricação  { get; set; }
+        public string Preço { get; set; }
+
+        public static List<CarroModel> CriarLista()
+        {
+            var lista = new List<CarroModel>();
+            lista.Add(new CarroModel() { Nome = "Rolls-Royce Boat Tail", Cor = "Azul", Fabricação = DateTime.Parse("2013/02/23") , Preço= "R$142.774.800" });
+            lista.Add(new CarroModel() { Nome = "Bugatti La Voiture Noire", Cor = "Cinza", Fabricação = DateTime.Parse ("2005/10/02"), Preço = "R$ 65.268.480", });
+            lista.Add(new CarroModel() { Nome = "Bugatti Centodieci", Cor = "Branco", Fabricação = DateTime.Parse("2019/05/25"), Preço = "R$ 45.891.900" });
+            lista.Add(new CarroModel() { Nome = "Mercedes-Maybach Exelero", Cor = "Preto", Fabricação = DateTime.Parse("2004/04/09"), Preço = "R$ 40.792.800" });
+
+
+
+
+            return lista;
+        }
+
+
+
+    }
+}
